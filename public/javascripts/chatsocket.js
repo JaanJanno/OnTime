2b521004f5d302e79f1632711adb76ff6529d5c9
JaanJanno <jaan911@ut.ee>
2014-03-30 23:56:22 +0300

    Websocket chatile

--- public/javascripts/chatsocket.js
+++ public/javascripts/chatsocket.js
@@ -8,6 +8,7 @@ function writeToChat(message)
 
 if ("WebSocket" in window)
 {
+	alert("ws"+document.URL.substring(4, document.URL.length-4)+"chat")
 	var ws = new WebSocket("ws"+document.URL.substring(4, document.URL.length-4)+"chat");
 	
 	ws.onmessage = function (evt) 
