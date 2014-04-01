dce961aa180cde6f336412d96bde33929f247cd2
Kristiina <kristiina.ilves@gmail.com>
2014-04-01 16:23:14 +0300

    log in töötab vastavalt sellele, kas scriptid on lubatud

--- public/javascripts/login-popup.js
+++ public/javascripts/login-popup.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $(".popup_button").click(function() {
+    $(".popup_button").click(function (event) {
+        event.preventDefault();
 		$(".popup").show();
 		$(".sulge_popup").click(function() {
 			$(".popup").hide();
