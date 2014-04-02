d055e884372e0106028a80ad39161f20c15ac92d
Jaan Janno <jaan911@ut.ee>
2014-04-02 14:38:20 +0300

    Veidi testandmeid juurde.

--- app/Global.java
+++ app/Global.java
@@ -7,6 +7,8 @@ import controllers.GameController;
 import models.*;
 import models.game.Terrain;
 import models.game.TerrainObject;
+import models.game.Tribe;
+import models.game.events.SpecialEvent;
 
 import java.util.*;
 
@@ -21,6 +23,15 @@ public class Global extends GlobalSettings {
         if (Terrain.find.findRowCount() == 0) {
         	GameController.mainTerrain = Terrain.initTerrain(10, 10);
         	Terrain.randomizeArea(GameController.mainTerrain);
+        	
+        	User aurelius = User.find.byId("aurelius@rome.ee");
+        	Tribe uusTribe = new Tribe(aurelius.name + "'s tribe.");		
+			Ebean.save(uusTribe);   			
+			aurelius.tribe = uusTribe;
+			Ebean.update(aurelius);
+			for (int i = 0; i < 15; i++){
+				SpecialEvent.rollSpecialEvent(uusTribe);
+			}		
         }
         if (GameController.mainTerrain == null){
         	GameController.mainTerrain = Terrain.find.all().get(0);
