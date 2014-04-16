dba216fd5c7c12bd643e2fb9933f5fd25b785405
JaanJanno <jaan911@ut.ee>
2014-04-16 16:52:27 +0300

    cleanup

--- app/controllers/Instructions.java
+++ app/controllers/Instructions.java
@@ -1,14 +1,8 @@
 package controllers;
 
 import static play.data.Form.form;
-import models.ChatEvent;
 import models.User;
-import models.game.Tribe;
-import models.game.events.GameEventQuery;
-import models.game.events.SpecialEvent;
-import models.game.events.WarEvent;
 import play.mvc.Result;
-import controllers.game.TerrainStreamer;
 import views.html.*;
 
 public class Instructions extends Application {
@@ -24,5 +18,4 @@ public class Instructions extends Application {
 				kasutaja
 		)));
 	}
-
 }
